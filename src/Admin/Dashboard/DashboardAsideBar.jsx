4c9aeb53fbeae1da17ac2799532b25ac51d03bd3
Abu Kalam <akwebdev69@gmail.com>
2025-09-10 08:04:00 +0600

    first commit

---
+++ src/Admin/Dashboard/DashboardAsideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardAsideBar = () => {
+    return (
+        <div>
+            <h2>Dashboard Aside Bar</h2>
+        </div>
+    );
+};
+
+export default DashboardAsideBar;
